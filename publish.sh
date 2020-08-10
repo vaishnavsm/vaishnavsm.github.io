bf1e7168407769e5ca620ea385e76f9328368e8d
Vaishnav S Menon <vaishnavsm@gmail.com>
2020-08-10 22:20:49 +0530

    Clear

--- publish.sh
+++
@@ -1,5 +0,0 @@
-if [ "$1" != "" ]; then
-        bundle exec jekyll build && git add -A && git commit -m "$1" && git push origin master && cd _site &&git add -A && git commit -m "$1" && git push origin master && cd ..
-    else
-        echo "Please Provide A Commit Message!"
-    fi
